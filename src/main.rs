28b55a804cf30efe760d3920a81f9adc63be29ad
Szizoid <e.ivan5@ya.ru>
2026-07-02 01:59:52 +0300

    Raw event loop

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 mod input;
 
 fn main() {
-    let devices = input::evdev::find_physical_keyboards();
-    for (path, device) in devices {
-        println!("{:?}: {}", path, device.name().unwrap());
+    let mut devices = input::evdev::find_physical_keyboards();
+    let (_, mut device) = devices.pop().unwrap();
+    loop {
+        let events = device.fetch_events().unwrap();
+        for event in events {
+            println!("{:?}", event);
+        }
     }
 }
